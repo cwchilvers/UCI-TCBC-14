0f4695bf2824272e4cc3662e2bb7bb174caa2703
Chandler Chilvers <cwchilvers@gmail.com>
2023-07-10 12:48:45 -0700

    Seeding database now works properly.

--- src/db/seeds/seed.js
+++ src/db/seeds/seed.js
@@ -1,34 +1,37 @@
-const sequelize = require('../config/connection');
-const { User, Post } = require('../models');
+const sequelize = require('../../config/connection');
+const { User, Post, Comment } = require('../../models/models');
 
 const userData = require('./userData.json');
 const postData = require('./postData.json');
+const commentData = require('./commentData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
   // Seed users
-  await User.bulkCreate(userData, {
+  const users = await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
 
   // Seed posts, assign to random user
+  const posts = [];
   for (const post of postData) {
-    await Post.create({
+    const createdPost = await Post.create({
       ...post,
       user_id: users[Math.floor(Math.random() * users.length)].id,
     });
+    posts.push(createdPost);
   }
 
   // Seed posts, assign to random user and random post
-  for (const comment of commentData) {
-    await Comment.create({
+  await Comment.bulkCreate(
+    commentData.map((comment) => ({
       ...comment,
       user_id: users[Math.floor(Math.random() * users.length)].id,
       post_id: posts[Math.floor(Math.random() * posts.length)].id,
-    });
-  }
+    }))
+  );
 
   process.exit(0);
 };
