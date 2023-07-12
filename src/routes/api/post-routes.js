366d76ede7869e0e50c1c2cc9ccc70376170320d
Chandler Chilvers <cwchilvers@gmail.com>
2023-07-12 09:37:06 -0700

    Can now view a post and add a comment through requests, but not through website yet.

--- src/routes/api/post-routes.js
+++ src/routes/api/post-routes.js
@@ -1,16 +1,56 @@
 const router = require('express').Router();
-const { Post } = require('../../models/models');
+const { User, Post, Comment } = require('../../models/models');
 
 // CREATE new post
 router.post('/', async (req, res) => {
     try {
-
-        console.log(req.body);
-        
         const dbPostData = await Post.create({
             title: req.body.title,
             content: req.body.content,
             user_id: req.session.userId,
+        });
+
+        res.status(200).json(dbPostData);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+// CREATE new comment
+router.post('/:id', async (req, res) => {
+    try {
+        const dbCommentData = await Comment.create({
+            content: req.body.content,
+            user_id: req.session.userId,
+            post_id: req.body.post_id,
+        });
+
+        res.status(200).json(dbCommentData);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+// VIEW post
+router.get('/:id', async (req, res) => {
+    try {
+        const dbPostData = await Post.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+                {
+                    model: Comment,
+                    attributes: ['content', 'user_id', 'post_id'],
+                    include: {
+                        model: User,
+                        attributes: ['username'],
+                    },
+                },
+            ],
         });
 
         res.status(200).json(dbPostData);
